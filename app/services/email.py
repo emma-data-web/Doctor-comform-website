d571bbd43560f35355cab1c039de42f4251e34ce
emma-data-web <emmanuelnwankwo690@gmail.com>
2026-03-24 12:33:58 +0100

    qrcode pillow issues

--- app/services/email.py
+++ app/services/email.py
@@ -1,34 +1,20 @@
 from fastapi_mail import FastMail, MessageSchema, MessageType
-import base64
 from app.core.email import conf
-import os 
-import uuid
+
 
 async def send_ticket_email(to_email: str, qr_base64: str):
-    # Convert base64 to bytes
-    qr_bytes = base64.b64decode(qr_base64)
-
-    # Save QR to a temporary file
-    file_path = f"ticket_{uuid.uuid4()}.png"
-    with open(file_path, "wb") as f:
-        f.write(qr_bytes)
+    
+    html_content = f"""
+    <p>Here is your ticket. Scan the QR code at the event:</p>
+    <img src="data:image/png;base64,{qr_base64}" alt="Ticket QR"/>
+    """
 
     message = MessageSchema(
         subject="Your Ticket Is Ready!",
         recipients=[to_email],
-        body="""
-        <p>Here is your ticket. Scan the QR code at the event.</p>
-        """,
-        subtype=MessageType.html,
-        attachments=[file_path]  #  pass file path
+        body=html_content,
+        subtype=MessageType.html
     )
 
     fm = FastMail(conf)
-    try: 
-        await fm.send_message(message)
-    finally:
-        if os.path.exists(file_path):
-         os.remove(file_path)
-
-    # Optional: delete file after sending
-    os.remove(file_path)
+    await fm.send_message(message)
