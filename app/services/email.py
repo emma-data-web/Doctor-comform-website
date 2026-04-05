d003ed4f3d9772580e1d9fca3297e4784cc9791e
emma-data-web <emmanuelnwankwo690@gmail.com>
2026-04-05 19:19:33 +0100

    fixing the qrcode image not sending error

--- app/services/email.py
+++ app/services/email.py
@@ -1,19 +1,34 @@
 from fastapi_mail import FastMail, MessageSchema, MessageType
 from app.core.email import conf
+import base64
+#import io
 
 
 async def send_ticket_email(to_email: str, qr_base64: str):
     
-    html_content = f"""
-    <p>Here is your ticket. Scan the QR code at the event:</p>
-    <img src="data:image/png;base64,{qr_base64}" alt="Ticket QR"/>
+    html_content = """
+    <h2>Welcome to Brunch & Pray! 🎉</h2>
+    <p>Your ticket is confirmed. Please find your QR code attached.</p>
+    <p>Present it at the event entrance to get in.</p>
+    <p>See you there! </p>
     """
+
+    
+    qr_bytes = base64.b64decode(qr_base64)
 
     message = MessageSchema(
         subject="Your Ticket Is Ready!",
         recipients=[to_email],
         body=html_content,
-        subtype=MessageType.html
+        subtype=MessageType.html,
+        attachments=[
+            {
+                "file": qr_bytes,
+                "filename": "ticket_qr.png",
+                "mime_type": "image",
+                "mime_subtype": "png"
+            }
+        ]
     )
 
     fm = FastMail(conf)
