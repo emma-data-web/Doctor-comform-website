17aa0e0a97da9383c03d5a457f96c49ea6d1f4a8
emma-data-web <emmanuelnwankwo690@gmail.com>
2026-04-09 21:12:28 +0100

    fixed idle connections issues

--- app/dependencies.py
+++ app/dependencies.py
@@ -4,7 +4,11 @@ from app.core.config import settings
 
 
 engine = create_engine(
-    settings.DATABASE_URL 
+    settings.DATABASE_URL,
+    pool_pre_ping=True,        
+    pool_recycle=300,         
+    pool_size=5,               
+    max_overflow=10            
 )
 
 
