95ee891dae42afe7401abce8f75ef5354170f924
emma-data-web <emmanuelnwankwo690@gmail.com>
2026-04-14 07:23:08 +0100

    fixed the syntax error in dependencies

--- app/dependencies.py
+++ app/dependencies.py
@@ -12,7 +12,7 @@ engine = create_engine(
 )
 
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)cd 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 def get_db():
